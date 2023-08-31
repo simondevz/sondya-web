9c5f0c0793797d29d5da544f2e4ed40f0c7c1da2
lordscoba <lordscoba2tm@gmail.com>
2023-08-31 10:15:50 -0700

    status

---
+++ src/images/productdetails/index.ts
@@ -0,0 +1,7 @@
+export { default as ProductdetailImage1 } from "./detail-1.png";
+export { default as ProductdetailImage2 } from "./detail-2.png";
+export { default as ProductdetailImage3 } from "./detail-3.png";
+export { default as ProductdetailImage4 } from "./detail-4.png";
+export { default as ProductdetailImage5 } from "./detail-5.png";
+export { default as ProductdetailImage6 } from "./detail-6.png";
+export { default as ProductdetailImageMain } from "./detail-main.png";
